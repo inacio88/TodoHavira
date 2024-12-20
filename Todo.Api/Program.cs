cd4a1d27f4c5acf27503e2695ee52a353df477ec
inacio88 <inacio88@outlook.com>
2024-12-20 14:24:16 -0300

    master: migratiom inicial

--- Todo.Api/Program.cs
+++ Todo.Api/Program.cs
@@ -2,16 +2,26 @@ using Todo.Api.Common.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
-builder.AddConfiguration();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
+// var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+// builder.Services.AddDbContext<AppDbContext>(options =>
+// {
+//     options.UseSqlServer(ConnectionString);
+// });
+builder.AddConfiguration();
 builder.AddDataContexts();
 
-builder.AddDocumentation();
-builder.AddServices();
+var app = builder.Build();
+
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
-
-app.ConfigureDevEnviroment();
 
 app.Run();
